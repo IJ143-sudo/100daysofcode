798fd222dea52b1682bcebd787b0ecb133fed72d
IJ143-sudo <jiqra143@gmail.com>
2025-09-13 22:35:23 +0530

    Added Day24 Codes

---
+++ Day24/Day48.c
@@ -0,0 +1,28 @@
+/*Write a program to print the following pattern:
+1
+12
+123
+1234
+12345
+*/
+
+#include <stdio.h>
+
+int main()
+ {
+    int i, j;
+
+    // Outer loop for rows
+    for (i = 1; i <= 5; i++)
+     {
+        // Inner loop prints numbers from 1 to i
+        for (j = 1; j <= i; j++) 
+        
+        {
+            printf("%d", j);
+        }
+        printf("\n"); // move to next line after each row
+    }
+
+    return 0;
+}
